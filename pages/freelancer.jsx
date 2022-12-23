0c0a8663fac0a9dacbd47af5c1149e8a5d81117e
mbaytas <m@baytas.net>
2022-12-23 11:45:01 +0100

    test

--- pages/freelancer.jsx
+++ pages/freelancer.jsx
@@ -92,6 +92,7 @@ export default function Freelancer() {
           url="https://www.calendly.com/"
         >
           <svg
+            width="100%"
             viewBox="0 0 16 16"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
