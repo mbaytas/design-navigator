4667d257e43fc6e873f3e7f52323cd4ccc3df0bf
mbaytas <m@baytas.net>
2023-01-23 09:57:55 +0100

    fix window error

--- components/careerHeader.jsx
+++ components/careerHeader.jsx
@@ -39,53 +39,55 @@ export default function CareerHeader(props) {
         <p className="opacity-60 text-r1">{props.p2}</p>
 
         {/* SHARE */}
-        <div className="flex flex-col justify-center gap-1 mt-9">
-          <p className="text-r1 opacity-50 font-display font-medium text-center">
-            Would you friends agree? Share and find out!
-          </p>
-          <div className="flex items-center justify-center gap-1 px-3 mx-auto">
-            <a
-              href={`http://twitter.com/share?url=According%20to%20@designdisciplin's%20Design%20Navigator,%20I%20would%20do%20well%20as%20a%20${props.title}.%0A%0AWould%20you%20agree?%0A%0A${window.location.href}`}
-              target="_blank"
-              className="secret-button opacity-50"
-            >
-              <LogoTwitter size="32" />
-            </a>
-            <a
-              href={`https://www.linkedin.com/sharing/share-offsite/?url=${window.location.href}`}
-              target="_blank"
-              className="secret-button opacity-50"
-            >
-              <LogoLinkedin size="32" />
-            </a>
-            <a
-              href={`https://www.facebook.com/sharer/sharer.php?u=${window.location.href}`}
-              target="_blank"
-              className="secret-button opacity-50"
-            >
-              <LogoFacebook size="32" />
-            </a>
-            <button
-              className="relative secret-button group w-14 h-12 opacity-50"
-              onClick={() => {
-                navigator.clipboard.writeText(window.location.href);
-              }}
-            >
-              <LinkIcon
-                className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 group-hover:opacity-0 group-active:opacity-0 transition-opacity group-active:transition-none"
-                size="32"
-              />
-              <Copy
-                className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 opacity-0 group-hover:opacity-100 group-active:opacity-0 transition-opacity group-active:transition-none"
-                size="32"
-              />
-              <Checkmark
-                className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 opacity-0 group-active:opacity-100 transition-opacity group-active:transition-none"
-                size="32"
-              />
-            </button>
+        {window !== undefined && (
+          <div className="flex flex-col justify-center gap-1 mt-9">
+            <p className="text-r1 opacity-50 font-display font-medium text-center">
+              Would you friends agree? Share and find out!
+            </p>
+            <div className="flex items-center justify-center gap-1 px-3 mx-auto">
+              <a
+                href={`http://twitter.com/share?url=According%20to%20@designdisciplin's%20Design%20Navigator,%20I%20would%20do%20well%20as%20a%20${props.title}.%0A%0AWould%20you%20agree?%0A%0A${window.location.href}`}
+                target="_blank"
+                className="secret-button opacity-50"
+              >
+                <LogoTwitter size="32" />
+              </a>
+              <a
+                href={`https://www.linkedin.com/sharing/share-offsite/?url=${window.location.href}`}
+                target="_blank"
+                className="secret-button opacity-50"
+              >
+                <LogoLinkedin size="32" />
+              </a>
+              <a
+                href={`https://www.facebook.com/sharer/sharer.php?u=${window.location.href}`}
+                target="_blank"
+                className="secret-button opacity-50"
+              >
+                <LogoFacebook size="32" />
+              </a>
+              <button
+                className="relative secret-button group w-14 h-12 opacity-50"
+                onClick={() => {
+                  navigator.clipboard.writeText(window.location.href);
+                }}
+              >
+                <LinkIcon
+                  className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 group-hover:opacity-0 group-active:opacity-0 transition-opacity group-active:transition-none"
+                  size="32"
+                />
+                <Copy
+                  className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 opacity-0 group-hover:opacity-100 group-active:opacity-0 transition-opacity group-active:transition-none"
+                  size="32"
+                />
+                <Checkmark
+                  className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 opacity-0 group-active:opacity-100 transition-opacity group-active:transition-none"
+                  size="32"
+                />
+              </button>
+            </div>
           </div>
-        </div>
+        )}
       </motion.header>
     </>
   );
